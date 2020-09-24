fea65a577396cbdc3e28a4c7a04777e56a0199dd
5ohyun <66874455+5ohyun@users.noreply.github.com>
2020-09-25 04:19:26 +0900

    Add files via upload

--- 2020hackathon/streamapp/views.py
+++ 2020hackathon/streamapp/views.py
@@ -2,9 +2,17 @@ from django.http import request
 from django.shortcuts import render
 from django.http import JsonResponse
 from django.http.response import StreamingHttpResponse
-from streamapp.camera import ClassifyMove, IPWebCam, han_ClassifyMove
-from .models import Test
+from streamapp.camera import ClassifyMove, IPWebCam
+from streamapp.models import Test, Alcohol
 import random
+from django_pandas.io import read_frame
+
+
+
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.metrics.pairwise import cosine_similarity
+import pandas as pd
+import numpy as np
 
 # Create your views here.
 
@@ -41,18 +49,6 @@ def poker_open(request):
     return JsonResponse({}, status=204)
 
 
-def han_onoff(request):
-    if Test.objects.get(pk=13).state:
-        Test.objects.filter(pk=13).update(
-            state=0,
-        )
-    else:
-        Test.objects.filter(pk=13).update(
-            state=1,
-        )
-    return JsonResponse({}, status=204)
-
-
 def index(request):
     if not (Test.objects.filter(pk=1)):
         Test.objects.create(
@@ -84,36 +80,6 @@ def index(request):
             state=0,
         )
         # 내 카드 공개여부
-    if not (Test.objects.filter(pk=8)):
-        Test.objects.create(
-            state=0,
-        )
-        # 한가인기본
-    if not (Test.objects.filter(pk=9)):
-        Test.objects.create(
-            state=0,
-        )
-        # 한가인 짠
-    if not (Test.objects.filter(pk=10)):
-        Test.objects.create(
-            state=0,
-        )
-        # 한가인 마신다
-    if not (Test.objects.filter(pk=11)):
-        Test.objects.create(
-            state=0,
-        )
-        # 한가인 하트
-    if not (Test.objects.filter(pk=12)):
-        Test.objects.create(
-            state=0,
-        )
-        # 사용자 상태
-    if not (Test.objects.filter(pk=13)):
-        Test.objects.create(
-            state=0,
-        )
-        # 한가인켜기끄기
     return render(request, "streamapp/home.html")
 
 
@@ -134,9 +100,57 @@ def camera_feed(request):
         gen(ClassifyMove()), content_type="multipart/x-mixed-replace; boundary=frame"
     )
 
-
-def han_camera_feed(request):
+def camera_feed(request):
     return StreamingHttpResponse(
-        gen(han_ClassifyMove()),
-        content_type="multipart/x-mixed-replace; boundary=frame",
+        gen(ClassifyMove()), content_type="multipart/x-mixed-replace; boundary=frame"
     )
+
+
+def alcohol_recommend(request):
+    beer = Alcohol.objects.all()
+    df0 = read_frame(beer)
+
+    beer = df0.loc[
+        :,
+            ["nation",
+            "name",
+            "alcohol_type",
+            "tag",
+            "alcohol",
+        ],
+    ]
+
+    beer['tag'] = beer['tag'].str.replace(',',' ')
+
+    tfidf_vec = TfidfVectorizer(ngram_range=(1, 3))
+    tfidf_matrix = tfidf_vec.fit_transform(beer['tag'])
+
+    genres_similarity = cosine_similarity(tfidf_matrix, tfidf_matrix)
+
+    similar_index = np.argsort(-genres_similarity)
+
+    input_beer = '이슬톡톡'
+
+    beer_index = beer[beer['name']==input_beer].index.values
+    similar_beer = similar_index[beer_index, :5]
+
+    similar_beer_index = similar_beer.reshape(-1)
+    top5 = pd.DataFrame(beer.iloc[similar_beer_index])
+    top5.reset_index(drop=True, inplace=True)
+    result_list = [] 
+
+    for i in range(5):
+            result_list.append(
+                {
+                    "nation": top5["nation"][i],
+                    "name": top5["name"][i],
+                    "alcohol_type": top5["alcohol_type"][i],
+                    "tag": top5["tag"][i],
+                    "alcohol": top5["alcohol"][i],
+                }
+            )
+    
+
+    context = {"result_list":result_list}
+
+    return render(request, "recommend.html", context)
