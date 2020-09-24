c9eea633cdbc3a41a5dd4b8aeb4e6bbd495e88f7
5ohyun <66874455+5ohyun@users.noreply.github.com>
2020-09-25 04:21:03 +0900

    Add files via upload

--- 2020hackathon/streamapp/urls.py
+++ 2020hackathon/streamapp/urls.py
@@ -2,12 +2,12 @@ from django.urls import path, include
 from streamapp import views
 
 
+
 urlpatterns = [
     path("", views.index, name="index"),
     path("camera_feed", views.camera_feed, name="camera_feed"),
     path("webcam_feed", views.webcam_feed, name="webcam_feed"),
-    path("han_camera_feed", views.han_camera_feed, name="han_camera_feed"),
     path("poker_start", views.poker_start, name="poker_start"),
     path("poker_open", views.poker_open, name="poker_open"),
-    path("han_onoff", views.han_onoff, name="han_onoff"),
+    path("alcohol_recommend", views.alcohol_recommend, name="alcohol_recommend"),
 ]
