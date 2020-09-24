ef624d07c2e2be6605f259039ee9d1c9be5c2502
5ohyun <66874455+5ohyun@users.noreply.github.com>
2020-09-25 04:20:15 +0900

    Add files via upload

--- 2020hackathon/streamapp/models.py
+++ 2020hackathon/streamapp/models.py
@@ -3,3 +3,10 @@ from django.db import models
 # Create your models here.
 class Test(models.Model):
     state = models.IntegerField(default=0)
+
+class Alcohol(models.Model) :
+    nation = models.TextField()
+    name = models.TextField()
+    alcohol_type = models.TextField()
+    tag = models.TextField()
+    alcohol = models.TextField()
